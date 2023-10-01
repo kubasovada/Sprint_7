7d444adf15841698af4c41c90bf5a4437e2c525d
Darya <kubasovada@mail.ru>
2023-10-02 00:57:50 +0300

    fixed CourierTest

--- src/main/java/CourierClient.java
+++ src/main/java/CourierClient.java
@@ -1,11 +1,23 @@
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 public class CourierClient {
     public static final String BASE_URI = "http://qa-scooter.praktikum-services.ru";
     public static final String COURIER_PATH = "/api/v1/courier";
+
+    public ValidatableResponse login(Credentials creds) {
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .body(creds)
+                .when()
+                .post(COURIER_PATH + "/login") // LOGIN
+                .then().log().all();
+    }
 
     public ValidatableResponse createCourier(Courier courier) {
         return given().log().all()
@@ -14,6 +26,16 @@ public class CourierClient {
                 .body(courier)
                 .when()
                 .post(COURIER_PATH) // CREATE
+                .then().log().all();
+    }
+
+    public ValidatableResponse delete(int courierId) {
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .body(Map.of("id", courierId))
+                .when()
+                .delete(COURIER_PATH + "/" + courierId) // DELETE
                 .then().log().all();
     }
 }
--- src/test/java/CourierTest.java
+++ src/test/java/CourierTest.java
@@ -1,39 +1,35 @@
-import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
 import org.junit.Test;
-
-import java.net.HttpURLConnection;
 
 import static io.restassured.RestAssured.given;
 
 public class CourierTest {
 
     private final CourierClient client = new CourierClient();
+    private final CourierAssertions check = new CourierAssertions();
+    protected int courierId;
+
+
+    @After
+    public void deleteCourier() {
+        ValidatableResponse delete = client.delete(courierId);
+        check.deletedSuccessfully(delete);
+
+    }
+
 
     @Test
     public void courier() {
         Courier courier = CourierGenerator.random();
-        //File json = new File("src/test/resources/newCard.json");
-        boolean created = client.createCourier(courier)
-                .assertThat()
-                .statusCode(HttpURLConnection.HTTP_CREATED)
-                .extract()
-                .path("ok");
+        ValidatableResponse response = client.createCourier(courier);
+        check.createdSuccessfully(response);
 
         Credentials creds = Credentials.from(courier);
-        int id = given().log().all()
-                .contentType(ContentType.JSON)
-                .baseUri(CourierClient.BASE_URI)
-                .body(creds)
-                .when()
-                .post(CourierClient.COURIER_PATH + "/login") // LOGIN
-                .then().log().all()
-                .assertThat()
-                .statusCode(HttpURLConnection.HTTP_OK)
-                .extract()
-                .path("id");
+        ValidatableResponse loginResponse = client.login(creds);
+        courierId = check.loggedInSuccessfully(loginResponse);
 
-        assert created;
-        assert id != 0;
+        assert courierId != 0;
     }
 
 }
